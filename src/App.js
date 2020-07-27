295b9654f892014752c45596b3463a4b0961fd0c
reguiguibrahim <67637958+reguiguibrahim@users.noreply.github.com>
2020-07-27 22:18:26 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './header/header';
+import Footer from './Footer/footer'
+import Main from './Main/main'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
